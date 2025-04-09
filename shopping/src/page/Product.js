1e782ea0cbc719335ebb320d83b52a384ecc9bf6
Doyoon <dlwhdgus482@naver.com>
2025-04-09 22:04:12 +0900

    url 수정

--- shopping/src/page/Product.js
+++ shopping/src/page/Product.js
@@ -5,7 +5,7 @@ import { Container, Row, Col } from "react-bootstrap";
 const Product = () => {
   const [productList, setProductList] = useState([]);
   const getProducts = async () => {
-    let url = `http://localhost:4000/products`;
+    let url = `https://my-json-server.typicode.com/DoYoonDev/react-study/products`;
     let response = await fetch(url);
     let data = await response.json();
     setProductList(data);
