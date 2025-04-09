0504d8bc8a7613a697748cb89b588689e6ecb2dd
이도윤 <dlwhdgus482@naver.com>
2025-04-09 15:54:30 +0900

    새로운 부분 추가

--- shopping/src/App.js
+++ shopping/src/App.js
@@ -1,9 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router';
+import Detail from './page/Detail';
+import Product from './page/Product';
+import Login from './page/Login';
+import Navbar from './component/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Product />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/detail/:id" element={<Detail />} />
+      </Routes>
+    </div>
   );
 }
 
