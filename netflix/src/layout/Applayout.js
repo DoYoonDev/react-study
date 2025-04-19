37630fdfc01d76f24b2c7f4e9cee10c9a758bfcf
Doyoon <dlwhdgus482@naver.com>
2025-04-19 23:54:37 +0900

    setting

--- netflix/src/layout/Applayout.js
+++ netflix/src/layout/Applayout.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Col } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
@@ -10,23 +11,18 @@ import { Outlet } from 'react-router-dom';
 const Applayout = () => {
   return (
     <div>
-      <Navbar expand="lg" className="" bg="dark" >
+      <Navbar expand="lg" className="" variant="dark">
         <Container fluid>
-          <Navbar.Brand href="#">
-            <img
-              alt=""
-              src="https://e7.pngegg.com/pngimages/119/342/png-clipart-logo-netflix-nasdaq-nflx-brand-television-copywriter-floor-television-text-thumbnail.png"
-            />
-          </Navbar.Brand>
+          <Navbar.Brand href="#" className='logo'>NETFLIX</Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav
               className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: '100px' }}
+              style={{ maxHeight: '100px'}}
               navbarScroll
             >
-              <Nav.Link href="#action1">Home</Nav.Link>
-              <Nav.Link href="#action2">Link</Nav.Link>
+              <Nav.Link href="/" className='nav-menu'>Home</Nav.Link>
+              <Nav.Link href="/movies" className='nav-menu'>Movies</Nav.Link>
             </Nav>
             <Form className="d-flex">
               <Form.Control
@@ -35,7 +31,7 @@ const Applayout = () => {
                 className="me-2"
                 aria-label="Search"
               />
-              <Button variant="outline-success">Search</Button>
+              <Button variant="outline-danger">Search</Button>
             </Form>
           </Navbar.Collapse>
         </Container>
